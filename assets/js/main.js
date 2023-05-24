4261a34e7751884d473b111bfbe36e0b3223792f
AdrianPol27 <adrianpolpeligrino27@gmail.com>
2023-05-24 21:54:18 +0800

    New Updates

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const submit = document.getElementsByClassName('form')[0];
+
+submit.addEventListener('submit', (e)=> {
+    e.preventDefault();
+
+    Email.send({
+        SecureToken : "7ed351fc-b024-4d3e-8181-0f94c4547f22",
+        To : 'info.appworksco@gmail.com',
+        From : email,
+        Subject : "Appworks Co. Inquiry",
+        Body : 'message'
+    }).then(
+      message => alert(message)
+    );
+})
